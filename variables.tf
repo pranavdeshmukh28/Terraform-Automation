e8383ce524a995f06584c44ace0f46e46e7061f8
pranavdeshmukh28 <pranavdeshmukh28@gmail.com>
2024-01-03 17:00:42 +0530

    amic

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-ami-0591e1c6a24d08458" 
+    default     = "ami-0591e1c6a24d08458" 
 }
 variable "versioning" {
     type        = bool
