21908662ebdfdb1105468638955fb23e5087142e
pranavdeshmukh28 <pranavdeshmukh28@gmail.com>
2024-01-03 15:56:00 +0530

    changes accourding to out need

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-21"
+    bucket = "mydev-tf-state-bucket-project-terraform-batch-21-pranav"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
