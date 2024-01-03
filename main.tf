7026fa8406f581071241aad58bd3c055b740eb77
pranavdeshmukh28 <pranavdeshmukh28@gmail.com>
2024-01-03 16:18:36 +0530

    minor change

--- main.tf
+++ main.tf
@@ -52,11 +52,11 @@ resource "aws_instance" "myFirstInstance" {
   }
 }
 
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
-  }
-}
+# # Create Elastic IP address
+# resource "aws_eip" "myFirstInstance" {
+#   vpc      = true
+#   instance = aws_instance.myFirstInstance.id
+# tags= {
+#     Name = "my_elastic_ip"
+#   }
+# }
